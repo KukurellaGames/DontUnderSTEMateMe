da81e4a868db2ef2cebcdbf5500bb659407f762c
oscar <oscar.sanchezsa@urjc.es>
2020-04-15 20:45:16 +0200

    Created Spikes traps animator

---
+++ DontUnderSTEMateMe/Assets/Scripts/elements/SpikesTrap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesTrap : MonoBehaviour
+{
+    [SerializeField] private Animator spikeAnimator;
+    public bool activUp;
+    public bool activDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //primera opcion
+
+        //segunda opcion
+    }
+
+    // Update is called once per frame
+    //si no se hace nada, por rendimiento se deberia eliminar
+    void Update()
+    {
+        if(activUp)
+        {
+            SpikesUp();
+            activUp = false;
+        }
+        if (activDown)
+        {
+            SpikesDown();
+            activDown = false;
+        }
+    }
+
+    public void SpikesUp()
+    {
+        spikeAnimator.SetTrigger("Up");
+    }
+    public void SpikesDown()
+    {
+        spikeAnimator.SetTrigger("Down");
+    }
+}
