c1136083b59ba7dc30e7649179b55cba1d46489b
Adrian Ollero <adrian.ollero@alu.uclm.es>
2020-04-24 19:17:04 +0200

    Unlocking ability working

---
+++ DontUnderSTEMateMe/Assets/Scripts/Abilities/PlayerAbilityManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilityManager : MonoBehaviour
+{
+    private AbilityTypes activeAbility;
+
+    // Ability
+    private bool canUseAbility = false;
+    private GameObject abilityGameObject;
+
+    private void Start()
+    {
+        activeAbility = AbilityTypes.UNLOCKING;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey("e"))
+        {
+            Interact();
+        }
+
+    }
+
+    /// <summary>
+    /// Checks when the player is able to use an ability
+    /// </summary>
+    /// <param name="other"></param>
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ability")
+        {
+            canUseAbility = true;
+            abilityGameObject = other.gameObject;
+            Debug.Log("Entro");
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ability")
+        {
+            canUseAbility = false;
+            abilityGameObject = null;
+            Debug.Log("Salgo");
+        }
+    }
+
+    /// <summary>
+    /// Function called by the user to use an ability
+    /// The ability that is used is the one in activeAbility
+    /// </summary>
+    public void Interact()
+    {
+        if (canUseAbility && checkAbilityType())
+        {
+            Ability ability = abilityGameObject.gameObject.GetComponent<Ability>();
+            ability.Interact();
+        }
+    }
+
+    private bool checkAbilityType()
+    {
+        Ability ability = abilityGameObject.GetComponent<Ability>();
+        if (ability == null)
+        {
+            Debug.Log("NULO");
+            return false;
+        }
+
+        if (ability.abilityType == activeAbility)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ DontUnderSTEMateMe/Assets/Scripts/Abilities/UnlockingAbility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockingAbility : Ability
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (animator == null)
+            Debug.LogError("[Unlocking Ability] Error getting animator reference");
+    }
+
+    #region IAbility
+    /// <summary>
+    /// This functions is called by the player when
+    /// interacts with the ability
+    /// </summary>
+    public override void Interact() 
+    {
+        animator.SetTrigger("Open");
+    }
+    #endregion
+}
