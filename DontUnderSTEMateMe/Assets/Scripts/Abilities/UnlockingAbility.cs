03cc82a4d69a08b5f4e711a2cd0c6da4c5ff5776
Adrian Ollero <adrian.ollero@alu.uclm.es>
2020-04-25 18:19:19 +0200

    Unlock ability working

--- DontUnderSTEMateMe/Assets/Scripts/Abilities/UnlockingAbility.cs
+++ DontUnderSTEMateMe/Assets/Scripts/Abilities/UnlockingAbility.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class UnlockingAbility : Ability
 {
+    [SerializeField] private ParticleSystem IdleParticles;
+    [SerializeField] private ParticleSystem ClimaxParticles;
+
     private Animator animator;
+
+    private bool IsLocked;
 
     private void Start()
     {
+        // Get animator reference
         animator = GetComponent<Animator>();
         if (animator == null)
             Debug.LogError("[Unlocking Ability] Error getting animator reference");
+
+        // Set idle particles to its initial state
+        IdleParticles.time = 5.0f;
+        for (int i = 0; i < IdleParticles.transform.childCount; i++)
+        {
+            ParticleSystem idleKeys = IdleParticles.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (idleKeys != null)
+            {
+                idleKeys.time = 5.0f;
+            }
+
+        }
+        IdleParticles.Play();
+
+        // Set door locked
+        IsLocked = false;
     }
 
     #region IAbility
@@ -20,7 +42,37 @@ public class UnlockingAbility : Ability
     /// </summary>
     public override void Interact() 
     {
-        animator.SetTrigger("Open");
+        StartCoroutine(InteractionRoutine());
     }
     #endregion
+
+    private IEnumerator InteractionRoutine()
+    {
+        if (!IsLocked)
+        {
+            IsLocked = true;
+            // Stop loop in idle particles - this will fade particles
+            var idleMain = IdleParticles.main;
+            idleMain.loop = false;
+            for (int i = 0; i < IdleParticles.transform.childCount; i++)
+            {
+                ParticleSystem idleKeys = IdleParticles.transform.GetChild(i).GetComponent<ParticleSystem>();
+                if (idleKeys != null)
+                {
+                    var idleKeysMain = idleKeys.main;
+                    idleKeysMain.loop = false;
+                }
+
+            }
+            yield return new WaitForSeconds(0.5f);
+
+            // Start climax particles
+            ClimaxParticles.Play();
+            yield return new WaitForSeconds(1.5f);
+
+            // Open door
+            animator.SetTrigger("Open");
+        }
+        
+    }
 }
