4827d1e44f50e05c84d81d0a9af6ab12ef3e73be
Enrique Garrido Pozo <enrique.garrido@gmail.com>
2020-05-26 18:45:54 +0200

    Arreglada la plataforma vertical

--- DontUnderSTEMateMe/Assets/Scripts/elements/Platform.cs
+++ DontUnderSTEMateMe/Assets/Scripts/elements/Platform.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Invector.vCharacterController;
 
 public class Platform : MonoBehaviour
 {
@@ -15,45 +16,58 @@ public class Platform : MonoBehaviour
     protected bool jump;
 
     protected Vector2 offset;
+    protected vThirdPersonController controller;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        controller = player.GetComponent<vThirdPersonController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.gameObject == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         jump = Input.GetButton("Jump");
 
+        
         //Aquí comprobar si el personaje anda, camina o salta
         if (horizontal != 0.0f || vertical != 0.0f || jump == true)
             mov = true;
         else
             mov = false;
 
-        if (isHorizontal)
+        if (isHorizontal && col)
             movementHorizontal();
-        else
+        if (!isHorizontal && col)
             movVertical();
 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        col = true;
+        if(collision.gameObject.tag == "Player")
+            col = true;
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        col = false;
+        if (collision.gameObject.tag == "Player")
+        {
+            col = false;
+            player.transform.SetParent(null);
+        }
     }
 
     private void movementHorizontal()
     {
+        //Tendremos que actualizar la posición cuando salte
         if (col == true && mov == false)
         {
             player.transform.position = new Vector3(this.transform.position.x + offset.x, this.transform.position.y, this.transform.position.z + offset.y);
@@ -63,13 +77,20 @@ public class Platform : MonoBehaviour
             offset.x = player.transform.position.x - this.transform.position.x;
             offset.y = player.transform.position.z - this.transform.position.z;
         }
+
+        Debug.Log(offset.x + "  " +offset.y);
+
     }
 
     private void movVertical()
     {
+        /*
         if (col == true && mov == false)
         {
             player.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
-        }
+        }*/
+
+        //Aquí hacer el parent
+        player.transform.SetParent(this.transform);
     }
 }
