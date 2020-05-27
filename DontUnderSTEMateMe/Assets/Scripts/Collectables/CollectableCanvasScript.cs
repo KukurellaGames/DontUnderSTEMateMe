ed7da9e7e59a86c11f8d2e0f24a90005f7e6ee64
Oscar <oscar.sanchezsa@urjc.es>
2020-05-27 19:58:00 +0200

    Canvas de coleccionable al hacer colision con su collider

---
+++ DontUnderSTEMateMe/Assets/Scripts/Collectables/CollectableCanvasScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableCanvasScript : MonoBehaviour
+{
+    [SerializeField] Canvas collectableCanvas;
+    [SerializeField] Button exitButton;
+    private void Start()
+    {
+        exitButton.onClick.AddListener(disableCanvas);
+    }
+    private void Update()
+    {
+        if(collectableCanvas.isActiveAndEnabled)
+        {
+            //Pause;
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                disableCanvas();
+            }
+        }
+    }
+
+    private void disableCanvas()
+    {
+        collectableCanvas.enabled = false;
+        Time.timeScale = 1f;
+    }
+}
---
+++ DontUnderSTEMateMe/Assets/Scripts/Collectables/CollectableSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableSystem : MonoBehaviour
+{
+    [SerializeField] protected string text;
+    [SerializeField] protected Canvas collectableCanvas;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Text collectableText = collectableCanvas.GetComponentInChildren<Text>();
+            collectableText.text = text;
+            collectableCanvas.enabled = true;
+            //añadir a informacion persistente
+            Time.timeScale = 0;
+            Destroy(this.gameObject);
+        }
+    }
+}
