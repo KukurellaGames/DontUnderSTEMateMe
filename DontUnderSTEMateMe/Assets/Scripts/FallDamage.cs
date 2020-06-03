4676eac772ad8fabecbd21cfa486e89ec74d527e
Alvaro <a.pavonal@alumnos.urjc.es>
2020-06-03 20:32:37 +0200

    Level 1 enemies + FallDamage fix + platforms fix

--- DontUnderSTEMateMe/Assets/Scripts/FallDamage.cs
+++ DontUnderSTEMateMe/Assets/Scripts/FallDamage.cs
@@ -5,54 +5,6 @@ using Invector.vCharacterController;
 
 public class FallDamage : MonoBehaviour
 {
-    /*
-    protected float lastPositionY = 0.0f;
-    protected float fallDistance = 0.0f;
-
-    protected vThirdPersonController controller;
-
-    protected Transform player;
-    [SerializeField]
-    private GameObject character;
-    //protected GameObject character;
-
-    [SerializeField]
-    protected float maxHeight;
-
-    private LifeManager _lifeManager;
-    private RespawnController _respawn;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        controller = this.GetComponent<vThirdPersonController>();
-        player = this.transform;
-        character = this.gameObject;
-        _lifeManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<LifeManager>();
-        _respawn = GameObject.FindGameObjectWithTag("GameController").GetComponent<RespawnController>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (lastPositionY > player.transform.position.y && !controller.isGrounded)
-            fallDistance += lastPositionY - player.transform.position.y;
-
-        lastPositionY = player.transform.position.y;
-
-        if(fallDistance >= maxHeight && controller.isGrounded)
-        {
-            lastPositionY = 0.0f;
-            fallDistance = 0.0f;
-            Destroy(this.gameObject);
-            GameObject charPrinc = Instantiate(character, _respawn.getRespawn().gameObject.transform.localPosition, _respawn.getRespawn().transform.localRotation);
-            _lifeManager.isDead();
-            Debug.Log("hola");
-            Debug.Log(_respawn.gameObject.transform.localPosition);
-        }
-    }
-    */
-
     protected float lastPositionY = 0.0f;
     protected float fallDistance = 0.0f;
 
@@ -84,10 +36,11 @@ public class FallDamage : MonoBehaviour
         {
             getInformation();
         }
-        if (lastPositionY > player.transform.position.y && !controller.isGrounded)
-            fallDistance += lastPositionY - player.transform.position.y;
 
-        lastPositionY = player.transform.position.y;
+        if (lastPositionY > player.position.y && !controller.isGrounded)
+            fallDistance += lastPositionY - player.position.y;
+
+        lastPositionY = player.position.y;
 
         if (fallDistance >= maxHeight && controller.isGrounded)
         {
@@ -97,6 +50,9 @@ public class FallDamage : MonoBehaviour
             GameObject charPrinc = Instantiate(character, _respawn.getRespawn().gameObject.transform.localPosition, _respawn.getRespawn().transform.localRotation);
             _lifeManager.isDead();
         }
+
+        if (controller.isGrounded)
+            fallDistance = 0.0f;
     }
 
     void getInformation()
