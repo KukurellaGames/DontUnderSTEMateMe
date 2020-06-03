8aa0556f7b89c88eb0cd3a45498f7949f469b552
Adrian Ollero <adrian.ollero@alu.uclm.es>
2020-06-03 18:30:27 +0200

    Lava particles

---
+++ DontUnderSTEMateMe/Assets/Materials/green_lava/ScrollableMaterial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollableMaterial : MonoBehaviour
+{
+    Material mat;
+    [SerializeField] protected float scrollSpeed = 0.2f;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float offset = Time.time * scrollSpeed;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
