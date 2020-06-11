92fa7bfc047fec63bf3d691b55306747021cd5f1
Enrique Garrido Pozo <enrique.garrido@gmail.com>
2020-06-11 19:20:58 +0200

    Fixing more

--- DontUnderSTEMateMe/Assets/Scripts/Utilities/Pause.cs
+++ DontUnderSTEMateMe/Assets/Scripts/Utilities/Pause.cs
@@ -6,10 +6,17 @@ public class Pause : MonoBehaviour
 {
     private bool active;
     private Canvas canvas;
+    private Canvas lifesUI;
+    private Canvas AbilityUI;
+    private Canvas ImageUI;
 
     // Start is called before the first frame update
     void Start()
     {
+        lifesUI = GameObject.Find("LifesUI")?.GetComponent<Canvas>();
+        AbilityUI = GameObject.Find("AbilityUI")?.GetComponent<Canvas>();
+        ImageUI = GameObject.Find("ImageLoader")?.GetComponent<Canvas>();
+        activateUI();
         canvas = GetComponent<Canvas>();
         canvas.enabled = false;
         DisabledCollectableList();
@@ -48,5 +55,19 @@ public class Pause : MonoBehaviour
     public void Exit()
     {
         Time.timeScale = 1f;
+    }
+
+    public void disableUI()
+    {
+        if (lifesUI != null) lifesUI.enabled = false;
+        if (AbilityUI != null) AbilityUI.enabled = false;
+        if (ImageUI != null) ImageUI.enabled = false;
+    }
+
+    public void activateUI()
+    {
+        if (lifesUI != null) lifesUI.enabled = true;
+        if (AbilityUI != null) AbilityUI.enabled = true;
+        if (ImageUI != null) ImageUI.enabled = true;
     }
 }
